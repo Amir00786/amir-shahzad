039a91a0e7e5af61fa768a71ace6cd3eb35fc5b1
Amir Shahzad <112079597+Amir00786@users.noreply.github.com>
2025-07-04 00:46:46 +0500

    feat: add code

---
+++ src/pages/banner/index.tsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react";
+import { Button } from "../../components/ui/button";
+import profile from "../../assets/profile.png";
+
+
+const roles = ["Frontend Developer", "Freelancer"];
+
+export const Banner = () => {
+    const [index, setIndex] = useState(0);
+    const [displayText, setDisplayText] = useState("");
+
+    useEffect(() => {
+        let currentText = roles[index];
+        let chars = currentText.split("");
+        let tempText = "";
+        let i = chars.length - 1;
+
+        // Typing animation (right to left)
+        const typeInterval = setInterval(() => {
+            tempText = chars[i] + tempText;
+            setDisplayText(tempText);
+            i--;
+
+            if (i < 0) {
+                clearInterval(typeInterval);
+
+                // Wait before deleting
+                setTimeout(() => {
+                    let j = chars.length - 1;
+
+                    const deleteInterval = setInterval(() => {
+                        tempText = tempText.slice(0, j);
+                        setDisplayText(tempText);
+                        j--;
+
+                        if (j < 0) {
+                            clearInterval(deleteInterval);
+                            setIndex((prev) => (prev + 1) % roles.length);
+                        }
+                    }, 100); // Character deletion speed
+                }, 2000); // Hold full text before deleting
+            }
+        }, 100); // Character typing speed
+
+        return () => clearInterval(typeInterval);
+    }, [index]);
+
+    return (
+        <div className="py-10 sm:py-20 md:py-60">
+            <div className="md:flex items-center -mx-4 justify-between">
+                <div className="md:w-1/2 w-full px-4">
+                    <h1 className="text-3xl lg:text-5xl flex flex-col gap-y-3 lg:gap-y-5 font-bold mb-3 lg:mb-5">
+                        <span className="block">Hello,</span>
+                        <span className="block">
+                            <i className="text-orange-500">I'm</i> Amir Shahzad
+                        </span>
+                        <span className="block text-orange-500 min-h-[60px] transition-all duration-300 ease-in-out">
+                            {displayText}
+                        </span>
+                    </h1>
+                    <p className="text-sm sm:text-base lg:text-lg mb-8">
+                        I am a passionate and dedicated frontend developer with over <strong>4<sup className="text-orange-500">+</sup></strong> {" "}
+                        years of experience, specializing in crafting user-friendly and high-performance web applications.
+                        With a strong foundation in HTML, CSS, JavaScript, Reactjs, Nextjs, Tailwindcss, and Boortstrap,
+                        I focus on building seamless, responsive interfaces
+                        that deliver exceptional user experiences.
+                    </p>
+                    <Button
+                        variant="outline"
+                        className="bg-orange-500 px-6 py-3 h-auto cursor-pointer text-white transition-all duration-300 ease-in-out border border-orange-500 text-xl hover:border-orange-500/80 hover:bg-orange-500/80 hover:text-white"
+                    >
+                        Download CV
+                    </Button>
+                </div>
+                <div className="md:w-1/2 w-full flex items-center justify-end px-4 pt-48 md:pt-0">
+                    <div className="relative w-full md:w-[450px] h-60 sm:h-80 lg:w-[590px] lg:h-96 rounded-4xl bg-orange-400/10">
+                        <div className="absolute -top-1/2 left-1/2 -translate-x-1/2 rounded-full w-[250px] h-[250px] sm:w-[300px] sm:h-[300px] xl:w-[450px] xl:h-[450px] bg-orange-500">
+                            <img src={profile} alt="profile" className="w-full shrink-0 bg-bottom rounded-full h-full object-cover" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
---
+++ src/pages/header/index.tsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+
+export const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const navLinks = [
+        { name: "Home", href: "/" },
+        { name: "About", href: "/" },
+        { name: "Services", href: "/" },
+        { name: "Projects", href: "/" },
+        { name: "Contact", href: "/" },
+    ];
+
+    return (
+        <header className="py-6">
+            <div className="flex justify-between items-center">
+                <strong>
+                    <a href="/" className="text-2xl lg:text-4xl flex items-center gap-2 font-bold text-white">
+                        Amir
+                        <span className="text-orange-500">Shahzad</span>
+                    </a>
+                </strong>
+                <div
+                    onClick={() => setIsOpen(!isOpen)}
+                    className="h-5 w-8 flex flex-col gap-y-1 items-center md:hidden"
+                >
+                    <div className="w-full h-1 bg-orange-500"></div>
+                    <div className="w-full h-1 bg-white"></div>
+                    <div className="w-full h-1 bg-orange-500"></div>
+                </div>
+                <nav className="hidden md:block">
+                    <ul className="flex items-center gap-x-6">
+                        {navLinks.map((link) => (
+                            <li key={link.name}>
+                                <a
+                                    href={link.href}
+                                    className="text-base lg:text-lg py-1 px-3 lg:px-5 hover:bg-orange-500 hover:rounded-full transition-all duration-300"
+                                >
+                                    {link.name}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </div>
+            <div
+                className={`fixed inset-0 z-30 md:hidden transition-opacity duration-300 ${isOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'
+                    }`}
+                onClick={() => setIsOpen(false)}
+            >
+                <div
+                    className={`absolute top-0 right-0 h-full w-64 bg-stone-800 text-white transform transition-transform duration-300 ${isOpen ? 'translate-x-0' : 'translate-x-full'
+                        }`}
+                    onClick={(e) => e.stopPropagation()}
+                >
+                    <div className="relative" onClick={() => setIsOpen(false)}>
+                        <span className="absolute right-8 w-1 h-6 bg-orange-500 top-7 rotate-[-45deg]"></span>
+                        <span className="absolute right-8 w-1 h-6 bg-orange-500 top-7 rotate-45"></span>
+                    </div>
+                    <ul className="flex flex-col gap-y-6 p-6 mt-20">
+                        {navLinks.map((link) => (
+                            <li key={link.name}>
+                                <a
+                                    href={link.href}
+                                    className="text-base lg:text-lg py-1 px-3 lg:px-5 hover:bg-orange-500 hover:rounded-full transition-all duration-300"
+                                    onClick={() => setIsOpen(false)} // Optional: close on link click
+                                >
+                                    {link.name}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </header>
+    )
+}

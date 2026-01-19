d5ea760b19212f4b353eca5fe3a06a596f614bbe
Amir Shahzad <amirshahzadvu91@gmail.com>
2026-01-19 22:06:43 +0500

    feat: add data

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,3 +1,7 @@
+import { Link } from "react-router-dom"
+import yelling1 from "../../assets/yelling-1.png"
+import { Button } from "../../components/ui/button"
+import { Eye } from "lucide-react"
 
 export const Projects = () => {
     return (
@@ -5,43 +9,62 @@ export const Projects = () => {
             <h1 className="text-4xl md:text-5xl mb-16 uppercase font-extrabold text-transparent stroke-orange-500 stroke-[2px]" data-aos="fade-up" data-aos-duration="2000">
                 My Work
             </h1>
-            <div className='flex gap-y-6 flex-wrap -mx-3'>
-                <div className='md:w-1/3 sm:w-1/2 w-full px-3' data-aos="fade-up" data-aos-duration="1000">
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                <div className='' data-aos="fade-up" data-aos-duration="1000">
+                    <div
+                        className='bg-white rounded-2xl p-4 cursor-pointer hover:shadow-xl'
+                        onClick={() => window.open('https://yellingant.com/', '_blank')}
+                    >
+                        <div className='flex flex-col gap-4'>
+                            <div className='w-full h-48 bg-gray-100 rounded-xl overflow-hidden flex items-center justify-center'>
+                                <img
+                                    src={yelling1}
+                                    alt="Yelling Ant"
+                                    className='w-full h-full object-cover transition-all duration-300 hover:scale-110'
+                                />
+                            </div>
+                            <div className='flex flex-col gap-2'>
+                                <h3 className='text-xl font-bold text-gray-800'>Yelling Ant</h3>
+                                <p className='text-sm text-gray-600'>Culture-centered social media network</p>
+                            </div>
+                            <Button 
+                            onClick={() => window.open('https://yellingant.com/', '_blank')}
+                            className='w-full bg-orange-500 text-white hover:bg-orange-500/80 hover:text-white'>
+                                <Eye className='w-4 h-4' />
+                                Live Preview
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+                <div className='' data-aos="fade-right" data-aos-duration="1000">
                     <div className='bg-white rounded-2xl p-6'>
                         <div className='flex items-center gap-2'>
                             <div className='w-12 h-12 bg-orange-500 rounded-full'></div>
                         </div>
                     </div>
                 </div>
-                <div className='md:w-1/3 sm:w-1/2 w-full px-3' data-aos="fade-right" data-aos-duration="1000">
+                <div className='' data-aos="fade-left" data-aos-duration="1000">
                     <div className='bg-white rounded-2xl p-6'>
                         <div className='flex items-center gap-2'>
                             <div className='w-12 h-12 bg-orange-500 rounded-full'></div>
                         </div>
                     </div>
                 </div>
-                <div className='md:w-1/3 sm:w-1/2 w-full px-3' data-aos="fade-left" data-aos-duration="1000">
+                <div className='' data-aos="zoom-in" data-aos-duration="1000">
                     <div className='bg-white rounded-2xl p-6'>
                         <div className='flex items-center gap-2'>
                             <div className='w-12 h-12 bg-orange-500 rounded-full'></div>
                         </div>
                     </div>
                 </div>
-                <div className='md:w-1/3 sm:w-1/2 w-full px-3' data-aos="zoom-in" data-aos-duration="1000">
+                <div className='' data-aos="flip-left" data-aos-duration="1000">
                     <div className='bg-white rounded-2xl p-6'>
                         <div className='flex items-center gap-2'>
                             <div className='w-12 h-12 bg-orange-500 rounded-full'></div>
                         </div>
                     </div>
                 </div>
-                <div className='md:w-1/3 sm:w-1/2 w-full px-3' data-aos="flip-left" data-aos-duration="1000">
-                    <div className='bg-white rounded-2xl p-6'>
-                        <div className='flex items-center gap-2'>
-                            <div className='w-12 h-12 bg-orange-500 rounded-full'></div>
-                        </div>
-                    </div>
-                </div>
-                <div className='md:w-1/3 sm:w-1/2 w-full px-3' data-aos="flip-right" data-aos-duration="1000">
+                <div className='' data-aos="flip-right" data-aos-duration="1000">
                     <div className='bg-white rounded-2xl p-6'>
                         <div className='flex items-center gap-2'>
                             <div className='w-12 h-12 bg-orange-500 rounded-full'></div>
